980257241c08a1ceec2cd21cf232f5fa5517c7c8
nomomon <funisinfinity26@gmail.com>
2023-11-11 02:01:45 +0100

    Add new files and modify existing ones

--- train.py
+++ train.py
@@ -1,56 +1,26 @@
-# %% imports
+import numpy as np
+from tqdm import tqdm
 import torch
-from torch_geometric.data import DataLoader
+
 from sklearn.metrics import (
     confusion_matrix,
     f1_score,
     accuracy_score,
     precision_score,
-    recall_score,
-    roc_auc_score,
+    recall_score
+    # roc_auc_score
 )
-import numpy as np
-from tqdm import tqdm
-import os
-from dataset_featurizer import DepressionDataset
-from model import GNN
+
 import mlflow.pytorch
+from utils import bcolors
 
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+from torch_geometric.loader import DataLoader
+from dataset import DepressionDataset
+from model import GNN
 
+# TODO: generally speaking, what's up with this code clean this up
 
-def count_parameters(model):
-    return sum(p.numel() for p in model.parameters() if p.requires_grad)
-
-
-# %% Delete the previous proccessed dataset
-os.system("rm -rf data/processed")
-
-# %% Loading the dataset
-train_dataset = DepressionDataset(root="data/", filename="train.tsv")
-test_dataset = DepressionDataset(root="data/", filename="dev.tsv", test=True)
-
-# %% Loading the model
-model = GNN(feature_size=train_dataset[0].x.shape[1])
-model = model.to(device)
-print(f"Number of parameters: {count_parameters(model)}")
-model
-
-# %% Loss and Optimizer
-# use class weights to balance the classes
-weights = torch.tensor([3.0, 1.0, 9.0]).to(device)
-loss_fn = torch.nn.CrossEntropyLoss(weight=weights)
-optimizer = torch.optim.SGD(model.parameters(), lr=0.1, momentum=0.9)
-scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.95)
-
-
-# %% Prepare training
-NUM_GRAPHS_PER_BATCH = 256
-train_loader = DataLoader(train_dataset, batch_size=NUM_GRAPHS_PER_BATCH, shuffle=True)
-test_loader = DataLoader(test_dataset, batch_size=NUM_GRAPHS_PER_BATCH, shuffle=True)
-
-
-def train(epoch):
+def run_train_epoch(epoch):
     # Enumerate over the data
     all_preds = []
     all_labels = []
@@ -63,9 +33,8 @@ def train(epoch):
         # Passing the node features and the connection info
         pred = model(
             batch.x.float(),
-            None,  # it needs to be edge attr
             batch.edge_index,
-            batch.batch,
+            batch.batch
         )
 
         # Calculating the loss and gradients
@@ -78,20 +47,18 @@ def train(epoch):
         all_labels.append(batch.y.cpu().detach().numpy())
     all_preds = np.concatenate(all_preds).ravel()
     all_labels = np.concatenate(all_labels).ravel()
-    calculate_metrics(all_preds, all_labels, epoch, "train")
+    calculate_metrics(all_preds, all_labels, epoch, 'train')
     return loss
 
-
-def test(epoch):
+def run_test_epoch(epoch):
     all_preds = []
     all_labels = []
     for batch in test_loader:
         batch.to(device)
         pred = model(
             batch.x.float(),
-            None,  # it needs to be edge attr
             batch.edge_index,
-            batch.batch,
+            batch.batch
         )
         loss = torch.sqrt(loss_fn(pred, batch.y))
         all_preds.append(np.argmax(pred.cpu().detach().numpy(), axis=1))
@@ -99,70 +66,88 @@ def test(epoch):
 
     all_preds = np.concatenate(all_preds).ravel()
     all_labels = np.concatenate(all_labels).ravel()
-    calculate_metrics(all_preds, all_labels, epoch, "test")
+    calculate_metrics(all_preds, all_labels, epoch, 'test')
     return loss
 
 
 def calculate_metrics(y_pred, y_true, epoch, type):
-    print(f"\n Confusion matrix: \n {confusion_matrix(y_pred, y_true)}")
-    print(f"Micro F1 Score: {f1_score(y_pred, y_true, average='micro')}")
-    print(f"Accuracy: {accuracy_score(y_pred, y_true)}")
-    print(f"Micro Precision: {precision_score(y_pred, y_true, average='micro')}")
-    print(f"Micro Recall: {recall_score(y_pred, y_true, average='micro')}")
-    try:
-        roc = roc_auc_score(y_pred, y_true)
-        print(f"ROC AUC: {roc}")
-        mlflow.log_metric(key=f"ROC-AUC-{type}", value=float(roc), step=epoch)
-    except:
-        mlflow.log_metric(key=f"ROC-AUC-{type}", value=float(0), step=epoch)
-        print(f"ROC AUC: notdefined")
+    print(f"""
+    Accuracy: {accuracy_score(y_pred, y_true)}
+    Confusion matrix: \n {confusion_matrix(y_pred, y_true)}
+    Micro Precision: {precision_score(y_pred, y_true, average='micro')}
+    Micro Recall: {recall_score(y_pred, y_true, average='micro')}
+    Micro F1 Score: {f1_score(y_pred, y_true, average='micro')}
+    """, end="")
+
+    mlflow.log_metric(f"{type}_micro_f1", f1_score(y_pred, y_true, average='micro'), step=epoch)
+    mlflow.log_metric(f"{type}_accuracy", accuracy_score(y_pred, y_true), step=epoch)
+    mlflow.log_metric(f"{type}_micro_precision", precision_score(y_pred, y_true, average='micro'), step=epoch)
+    mlflow.log_metric(f"{type}_micro_recall", recall_score(y_pred, y_true, average='micro'), step=epoch)
 
 
-# %% Run the training
-train_losses = []
-test_losses = []
 
-with mlflow.start_run() as run:
-    for epoch in range(500):
-        # Training
-        model.train()
-        loss = train(epoch=epoch)
-        loss = loss.detach().cpu().numpy()
-        print(f"Epoch {epoch} | Train Loss {loss}")
-        mlflow.log_metric(key="Train loss", value=float(loss), step=epoch)
-        train_losses.append([epoch, loss])
+if __name__ == "__main__":
+    # os.system('rm -rf data/processed')
 
-        # Testing
-        model.eval()
-        if epoch % 5 == 0:
-            loss = test(epoch=epoch)
+    # Model hyperparameters
+    # TODO: find these hyperparameters
+    # TODO: add these hyperparameters to the mlflow log
+    model_params = {
+        "model_embedding_size": 32,
+        "model_attention_heads": 4,
+        "model_layers": 3,
+        "model_dropout_rate": 0.1,
+        "model_top_k_ratio": 0.5,
+        "model_dense_neurons": 64,
+    }
+
+    # Load the data
+    train_dataset = DepressionDataset(root='data/', filename='train.tsv')
+    test_dataset = DepressionDataset(root='data/', filename='dev.tsv', prefix="dev")
+
+    # Create the data loaders
+    NUM_GRAPHS_PER_BATCH = 256
+    train_loader = DataLoader(train_dataset, batch_size=NUM_GRAPHS_PER_BATCH, shuffle=True)
+    test_loader = DataLoader(test_dataset, batch_size=NUM_GRAPHS_PER_BATCH, shuffle=True)
+
+    # Use GPU if available
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    
+    # Initialize the model
+    model = GNN(
+        feature_size=train_dataset[0].x.shape[1],
+        num_classes=3,
+        model_params=model_params
+    )
+    model = model.to(device)
+    print("Number of parameters:", model.count_parameters())
+
+    # Initialize the loss function and optimizer
+    weights = torch.tensor([1.0, .25, 1.5]).to(device) # TODO: extract this ratio calculation
+    loss_fn = torch.nn.CrossEntropyLoss(weight=weights)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001, weight_decay=5e-4)
+    scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.95)
+    
+    # Run the training loop
+    with mlflow.start_run() as run:
+        for epoch in range(500):
+            # Training
+            print(bcolors.BOLD, f"Epoch {epoch} | TRAIN", bcolors.ENDC, bcolors.OKBLUE)
+            model.train()
+            loss = run_train_epoch(epoch=epoch)
             loss = loss.detach().cpu().numpy()
-            print(f"Epoch {epoch} | Test Loss {loss}")
-            mlflow.log_metric(key="Test loss", value=float(loss), step=epoch)
-            test_losses.append([epoch, loss])
+            print(f"Train Loss {loss}", bcolors.ENDC)
+            mlflow.log_metric(key='Train loss', value=float(loss), step=epoch)
 
-        scheduler.step()
-    print("Done.")
+            # Testing
+            model.eval()
+            if epoch % 5 == 0:
+                print(bcolors.BOLD, f"Epoch {epoch} | TEST", bcolors.ENDC, bcolors.OKGREEN)
+                loss = run_test_epoch(epoch=epoch)
+                loss = loss.detach().cpu().numpy()
+                print(f"Test Loss {loss}", bcolors.ENDC)
+                mlflow.log_metric(key='Test loss', value=float(loss), step=epoch)
+            scheduler.step()
+    print('Done.')
 
-
-# %% Save the model
-mlflow.pytorch.log_model(model, "model")
-
-# %% Make the train/validation loss plot
-import matplotlib.pyplot as plt
-
-
-def plot_loss(losses, title):
-    plt.gca().plot([x[0] for x in losses], [x[1] for x in losses], title=title)
-
-
-plt.figure(figsize=(10, 5))
-plot_loss(train_losses, "Train loss")
-plot_loss(test_losses, "Test loss")
-plt.legend(["Train", "Test"])
-plt.xlabel("Epoch")
-plt.ylabel("Loss")
-plt.title("Loss vs Epoch")
-plt.savefig("loss.png")
-
-# %%
+    mlflow.pytorch.log_model(model, 'model')
