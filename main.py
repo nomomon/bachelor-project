eb6b3e06538d7e36861b1f1a2472525f04e7f65f
nomomon <funisinfinity26@gmail.com>
2023-12-19 10:24:30 +0100

    Add seaborn library for confusion matrix vis

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import torch
 from tqdm import tqdm
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 from torch.utils.data import WeightedRandomSampler
 from torch_geometric.loader import DataLoader
@@ -10,8 +11,7 @@ from torch_geometric.loader import DataLoader
 from sklearn.metrics import (
     f1_score,
     accuracy_score,
-    precision_score,
-    recall_score
+    confusion_matrix
 )
 from src.data.utils import read_tsv
 
@@ -36,10 +36,9 @@ if __name__ == '__main__':
     print("Getting class weights...", end=' ')
     label_to_class = {"not depression": 0, "moderate": 1, "severe": 2}
     labels = read_tsv('data/silver/train.tsv')["label"].apply(lambda x: label_to_class[x]).values
-    class_weights = 1 / np.bincount(labels)
+    class_weights = 1 - np.bincount(labels) / len(labels)
     class_weights = class_weights / class_weights.sum()
     print(class_weights)
-
     sample_weights = class_weights[labels]
     sample_weights = torch.FloatTensor(sample_weights).to(device)
 
@@ -48,7 +47,7 @@ if __name__ == '__main__':
     sampler = WeightedRandomSampler(
         weights=sample_weights,
         num_samples=len(sample_weights),
-        replacement=True
+        replacement=False
     )
 
     # Get loaders
@@ -75,16 +74,21 @@ if __name__ == '__main__':
         'train_loss': [],
         'train_acc': [],
         'train_f1': [],
+        'train_cm': [],
         'valid_loss': [],
         'valid_acc': [],
         'valid_f1': [],
+        'valid_cm': [],
         'valid_x': [],
     }
 
+    # Start training
+    print("Start training...")
     for epoch in range(0, 100):
         step_loss = 0
         step_acc = 0
         step_f1 = 0
+        step_cm = None
         
         model.train()
         for batch in tqdm(train_loader, desc=f'Epoch {epoch:3d} | Train'):
@@ -102,11 +106,16 @@ if __name__ == '__main__':
 
             pred = out.argmax(dim=1)
             acc = accuracy_score(batch.y.cpu(), pred.cpu())
-            f1 = f1_score(batch.y.cpu(), pred.cpu(), average='weighted')
+            f1 = f1_score(batch.y.cpu(), pred.cpu(), average='macro')
+
+            if torch.any(pred.cpu() > 2):
+                print("Error: pred > 2", pred.cpu())
+                quit()
 
             step_loss += loss.item()
             step_acc += acc
             step_f1 += f1
+            step_cm = confusion_matrix(batch.y.cpu(), pred.cpu()) if step_cm is None else step_cm + confusion_matrix(batch.y.cpu(), pred.cpu())
 
         step_loss /= len(train_loader)
         step_acc /= len(train_loader)
@@ -115,12 +124,14 @@ if __name__ == '__main__':
         hist['train_loss'].append(step_loss)
         hist['train_acc'].append(step_acc)
         hist['train_f1'].append(step_f1)
+        hist['train_cm'].append(step_cm)
 
 
         if epoch % 5 == 0:
             step_loss = 0
             step_acc = 0
             step_f1 = 0
+            step_cm = None
 
             model.eval()
             with torch.no_grad():
@@ -140,6 +151,7 @@ if __name__ == '__main__':
                     step_loss += loss.item()
                     step_acc += acc
                     step_f1 += f1
+                    step_cm = confusion_matrix(batch.y.cpu(), pred.cpu()) if step_cm is None else step_cm + confusion_matrix(batch.y.cpu(), pred.cpu())
 
             step_loss /= len(valid_loader)
             step_acc /= len(valid_loader)
@@ -148,30 +160,43 @@ if __name__ == '__main__':
             hist['valid_loss'].append(step_loss)
             hist['valid_acc'].append(step_acc)
             hist['valid_f1'].append(step_f1)
+            hist['valid_cm'].append(step_cm)
             hist['valid_x'].append(epoch)
 
         # plot the training loss and accuracy
-        fig, axs = plt.subplots(1, 3, figsize=(18, 5))
-        axs[0].plot(hist['train_loss'], label='train loss')
-        axs[0].plot(hist['valid_x'], hist['valid_loss'], label='valid loss')
-        axs[0].set_xlabel('Epoch')
-        axs[0].set_ylabel('Loss')
-        axs[0].legend(loc='upper right')
-        axs[0].set_title('Loss')
+        fig, axs = plt.subplots(2, 3, figsize=(18, 12))
+        axs[0][0].plot(hist['train_loss'], label='train loss')
+        axs[0][0].plot(hist['valid_x'], hist['valid_loss'], label='valid loss')
+        axs[0][0].set_xlabel('Epoch')
+        axs[0][0].set_ylabel('Loss')
+        axs[0][0].legend(loc='upper right')
+        axs[0][0].set_title('Loss')
         
-        axs[1].plot(hist['train_acc'], label='train acc')
-        axs[1].plot(hist['valid_x'], hist['valid_acc'], label='valid acc')
-        axs[1].set_xlabel('Epoch')
-        axs[1].set_ylabel('Accuracy')
-        axs[1].legend(loc='upper right')
-        axs[1].set_title('Accuracy')
+        axs[0][1].plot(hist['train_acc'], label='train acc')
+        axs[0][1].plot(hist['valid_x'], hist['valid_acc'], label='valid acc')
+        axs[0][1].set_xlabel('Epoch')
+        axs[0][1].set_ylabel('Accuracy')
+        axs[0][1].legend(loc='upper right')
+        axs[0][1].set_title('Accuracy')
 
-        axs[2].plot(hist['train_f1'], label='train f1')
-        axs[2].plot(hist['valid_x'], hist['valid_f1'], label='valid f1')
-        axs[2].set_xlabel('Epoch')
-        axs[2].set_ylabel('F1')
-        axs[2].legend(loc='upper right')
-        axs[2].set_title('F1')
+        axs[0][2].plot(hist['train_f1'], label='train f1')
+        axs[0][2].plot(hist['valid_x'], hist['valid_f1'], label='valid f1')
+        axs[0][2].set_xlabel('Epoch')
+        axs[0][2].set_ylabel('F1')
+        axs[0][2].legend(loc='upper right')
+        axs[0][2].set_title('F1')
+
+        sns.heatmap(hist['train_cm'][-1], annot=True, fmt='d', ax=axs[1][0])
+        axs[1][0].set_xlabel('Predicted')
+        axs[1][0].set_ylabel('Actual')
+        axs[1][0].set_title(f'Train Confusion Matrix (Epoch {epoch:3d})')
+        axs[1][0].set_aspect('equal')
+
+        sns.heatmap(hist['valid_cm'][-1], annot=True, fmt='d', ax=axs[1][1])
+        axs[1][1].set_xlabel('Predicted')
+        axs[1][1].set_ylabel('Actual')
+        axs[1][1].set_title(f'Valid Confusion Matrix (Epoch {hist["valid_x"][-1]:3d})')
+        axs[1][1].set_aspect('equal')
 
         fig.savefig('results.png')
         plt.close(fig)
